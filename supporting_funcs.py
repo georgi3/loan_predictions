c80dffb8de6c6991ab040e7695bfa56eb03cb0eb
George Marchenko <george.marchenko44@gmail.com>
2022-05-20 14:39:42 -0400

    added ClfSwitcher class

--- supporting_funcs.py
+++ supporting_funcs.py
@@ -1,11 +1,17 @@
 import numpy as np
-
+from sklearn.base import BaseEstimator
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import BernoulliNB
 
 # data engineering
 def comb_income(data, cols):
     data['CombinedIncome'] = data[cols].sum(axis=1)
     data = data.drop(cols, axis=1)
     return np.log(data)
+
+
+def to_drop(data, cols):
+    return data[:, len(cols):]  # (cols, axis=1, inplace=True)
 
 
 # data separation
@@ -28,3 +34,25 @@ class ToDenseTransformer:
 
     def fit(self, X, y=None):
         return self
+
+
+class ClfSwitcher(BaseEstimator):
+    def __init__(self, estimator=BernoulliNB()):
+        self.estimator = estimator
+
+    def fit(self, X, y=None):
+        self.estimator.fit(X, y)
+        return self
+
+    def predict(self, X, y=None):
+        return self.estimator.predict(X)
+
+    def predict_proba(self, X):
+        return self.estimator.predict_proba(X)
+
+    def score(self, X, y):
+        return self.estimator.score(X, y)
+
+
+
+
